e3e385f2d49d6f68c7d8c044ee4de5a90ec53e0a
SharMelde <njugunashar26@gmail.com>
2025-09-15 11:51:00 +0300

    Re-push entire project to overwrite existing repo

--- src/features/organizations/db/cache/organizations.ts
+++ src/features/organizations/db/cache/organizations.ts
@@ -1,15 +1,15 @@
-import { getGlobalTag, getIdTag } from "@/lib/dataCache";
-import { revalidateTag } from "next/cache";
+import { getGlobalTag, getIdTag } from "@/lib/dataCache"
+import { revalidateTag } from "next/cache"
 
 export function getOrganizationGlobalTag() {
-    return getGlobalTag("Organization")
+  return getGlobalTag("organizations")
 }
 
 export function getOrganizationIdTag(id: string) {
-    return getIdTag("Organization", id)
+  return getIdTag("organizations", id)
 }
 
 export function revalidateOrganizationCache(id: string) {
-    revalidateTag(getOrganizationGlobalTag())
-    revalidateTag(getOrganizationIdTag(id))
+  revalidateTag(getOrganizationGlobalTag())
+  revalidateTag(getOrganizationIdTag(id))
 }
